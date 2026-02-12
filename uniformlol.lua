426e4c9d9013bb3ce3e945553c5cac3c1f73e6f8
CryWinkleSam <uncync82@gmail.com>
2026-02-11 19:08:52 -0800

    Update uniformlol.lua

--- uniformlol.lua
+++ uniformlol.lua
@@ -22,7 +22,7 @@ local Cfg = {
 }
 wait(1)
 local function CheckServerCode() -- dont change
-    local blockedCodes = { "lanrp", "Joinmrpp", "Tnzrp", "test", "DRIFTED", "HAMP", "Txrpvc" }
+    local blockedCodes = { "lanrp", "Joinmrpp", "Tnzrp", "test", "DRIFTED", "HAMP", "ncsrrp" }
     
     for _, code in ipairs(blockedCodes) do
         if JoinCode == code then
