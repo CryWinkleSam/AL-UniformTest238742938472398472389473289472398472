a891deab2e931caf4350e402d7a046d5d361d1a4
CryWinkleSam <uncync82@gmail.com>
2026-02-28 23:29:50 -0800

    add wl

--- dddliv.lua
+++ dddliv.lua
@@ -26,7 +26,7 @@ local function WaitForReserve()
 end
 
 local function CheckServerCode()
-	local blockedCodes = { "lanrp", "Joinmrpp", "Tnzrp", "test", "DRIFTED", "HAMP", "ncsrppp", "tcrrp" }
+	local blockedCodes = { "lanrp", "Joinmrpp", "Tnzrp", "test", "DRIFTED", "HAMP", "ncsrppp", "tcrrp", "crpnc" }
 
 	for _, code in ipairs(blockedCodes) do
 		if JoinCode == code then
